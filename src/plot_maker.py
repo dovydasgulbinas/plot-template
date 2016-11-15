53e24837a51a1f611b8c54a1153b8d9904c64382
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-15 20:25:08 +0200

    adding reference for developing in linux

---
+++ src/plot_maker.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import pandas
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+
+#data = pandas.read_csv('transfer-t2-g13-cleaned.csv', sep=',')
+
