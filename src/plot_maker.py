3ba2b00cd884395e775ad0c96d89ba68f5d4cfa7
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-15 23:11:26 +0200

    adding legend suport base on csv file header

--- src/plot_maker.py
+++ src/plot_maker.py
@@ -7,24 +7,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import spline
 
-
 data_file_path = 'real-data.csv'
 value_separator = ','
 title = 'ąčęėi-xxx-įšųū'
 name_xaxis = 'I, (A)'
 name_yaxis = 'U, (V)'
+output_name = 'title-test.png'
+display_legend = True
 yscale = 'linear'
 xscale = 'linear'
-output_name = 'title-test.png'
+
 
 df = pandas.read_csv(data_file_path, value_separator)
 ncols = len(df.columns)
+headers = df.columns.values.tolist()
 print("Import success!")
 print(df.head())
 print("Number of columns: {}".format(ncols))
-print(title)
+print(headers)
 
-plt.title(title)
 plt.xscale(xscale)
 plt.yscale(yscale)
 
@@ -34,8 +35,6 @@ axes = fig.add_subplot(111)
 axes.grid()
 axes.set_xlabel(name_xaxis)
 axes.set_ylabel(name_yaxis)
-
-plt.title(title)
 
 # get all data & start ploting
 x = df[df.columns[0]]
@@ -47,7 +46,12 @@ for i in range(1, ncols):
     
     # interpolates y values based on smooth x
     ysmooth = spline(x, y, xsmooth)
-    axes.plot(xsmooth, ysmooth) 
+    axes.plot(xsmooth, ysmooth, label=headers[i]) 
     
 #export to .png
+if display_legend:
+    plt.legend(loc='best')
+
+# title rendering works unpredictably 
+plt.title(title)
 plt.savefig(output_name)
