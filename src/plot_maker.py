157065ce100925390a9e09e2c73cb43b728180c1
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-15 21:51:55 +0200

    adding basic plot generation

--- src/plot_maker.py
+++ src/plot_maker.py
@@ -5,5 +5,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-#data = pandas.read_csv('transfer-t2-g13-cleaned.csv', sep=',')
+data_file_path = 'test.csv'
+name_xaxis = 'I, (A)'
+name_yaxis = 'U, (V)'
+output_name = 'test-test.png'
 
+df = pandas.read_csv(data_file_path, sep='\t')
+ncols = len(df.columns)
+print("Import success!")
+print(df.head())
+print("Number of columns: {}".format(ncols))
+
+fig = plt.figure()
+
+# Define axes
+axes = fig.add_subplot(111)
+axes.grid()
+axes.set_xlabel(name_xaxis)
+axes.set_ylabel(name_yaxis)
+
+# get all data & start ploting
+x = df[df.columns[0]]
+for i in range(1, ncols):
+    y = df[df.columns[i]]
+    axes.plot(x,y) 
+    
+#export to .png
+plt.savefig(output_name)
