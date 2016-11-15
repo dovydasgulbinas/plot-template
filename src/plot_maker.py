00187de3666ce809abbe66c57877f47eb9bf6acd
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-15 22:59:14 +0200

    adding title support

--- src/plot_maker.py
+++ src/plot_maker.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import pandas
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.animation as animation
+from scipy.interpolate import spline
 
-data_file_path = 'test.csv'
+
+data_file_path = 'real-data.csv'
+value_separator = ','
+title = 'ąčęėi-xxx-įšųū'
 name_xaxis = 'I, (A)'
 name_yaxis = 'U, (V)'
-output_name = 'test-test.png'
+yscale = 'linear'
+xscale = 'linear'
+output_name = 'title-test.png'
 
-df = pandas.read_csv(data_file_path, sep='\t')
+df = pandas.read_csv(data_file_path, value_separator)
 ncols = len(df.columns)
 print("Import success!")
 print(df.head())
 print("Number of columns: {}".format(ncols))
+print(title)
 
-fig = plt.figure()
+plt.title(title)
+plt.xscale(xscale)
+plt.yscale(yscale)
 
 # Define axes
+fig = plt.figure()
 axes = fig.add_subplot(111)
 axes.grid()
 axes.set_xlabel(name_xaxis)
 axes.set_ylabel(name_yaxis)
 
+plt.title(title)
+
 # get all data & start ploting
 x = df[df.columns[0]]
+xsmooth = np.array(x)
+xsmooth = np.linspace(xsmooth.min(), xsmooth.max(), 200)
+
 for i in range(1, ncols):
     y = df[df.columns[i]]
-    axes.plot(x,y) 
+    
+    # interpolates y values based on smooth x
+    ysmooth = spline(x, y, xsmooth)
+    axes.plot(xsmooth, ysmooth) 
     
 #export to .png
 plt.savefig(output_name)
