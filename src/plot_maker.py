25521909303958a4547414c9ccc4de05eea60941
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2016-11-20 12:00:21 +0200

    adding interpolation options

--- src/plot_maker.py
+++ src/plot_maker.py
@@ -14,10 +14,20 @@ parser = argparse.ArgumentParser(prog='Plotter',description='Plots .csv')
 parser.add_argument('data_file_name', help='File name')
 parser.add_argument('value_separator', help='The character that is used to denote how values are separated')
 parser.add_argument('--title', metavar='str', help='sets a figure title')
+parser.add_argument('--smooth-type', type=int, choices=range(0, 3),
+                    help='0 - no smoothing, 1 - spline smoothing')
+parser.add_argument('--interpolation-points', type=int, metavar='int', default=200,
+                    help='lets you to set a number of interpolation points for a smooth plot')
 
-#args = parser.parse_args(['test-data.csv', ',', '--title', 'go.nx'])
-args = parser.parse_args()
+args = parser.parse_args(['test-data.csv', ',', '--title', 'go.nx'])
+# args = parser.parse_args(['test-data.csv', ',', '--smooth-type', '1'])
+#args = parser.parse_args(['--help'])
 print(args)
+
+# TODO: Scan for cols with [x, xx ...] names. All colls with
+# TODO:
+# TODO:
+
 
 file_name = args.data_file_name
 value_separator = args.value_separator
@@ -25,8 +35,11 @@ title = args.title
 name_xaxis = 'I, (A)'
 name_yaxis = 'U, (V)'
 display_legend = True
+smooth_setting = args.smooth_type
+ninterpolation_points = args.interpolation_points
 yscale = 'linear'
 xscale = 'linear'
+
 
 df = pandas.read_csv(file_name, value_separator)
 ncols = len(df.columns)
@@ -35,6 +48,9 @@ print("Import success!")
 print(df.head())
 print("Number of columns: {}".format(ncols))
 print(headers)
+
+# FIXME: remove exit line
+exit()
 
 plt.xscale(xscale)
 plt.yscale(yscale)
@@ -49,7 +65,7 @@ axes.set_ylabel(name_yaxis)
 # get all data & start ploting
 x = df[df.columns[0]]
 xsmooth = np.array(x)
-xsmooth = np.linspace(xsmooth.min(), xsmooth.max(), 200)
+xsmooth = np.linspace(xsmooth.min(), xsmooth.max(), ninterpolation_points)
 
 for i in range(1, ncols):
     y = df[df.columns[i]]
